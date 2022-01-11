8262c3518d83c79ddde8767166b24d55c1902815
nicolas <nicolas.sabas@epitech.eu>
2022-01-11 22:57:28 +0100

    source

---
+++ src/components/Game.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <Players/>
+</template>
+
+<script>
+import Players from "./Players";
+export default {
+
+  components: {Players},
+  props: [
+      'players'
+  ],
+  name: "Game",
+  methods: {
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Players.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="players">
+    <h1>Enter players names :</h1>
+
+    <input type="text" placeholder="Nom" v-model="player_name">
+    <button @click="addPlayersName(player_name)" >Add</button>
+    <ul>
+      <li v-for="player in players" :key="player">{{ player }} <button @click="players.splice(players.indexOf(player), 1)" >delete</button></li>
+    </ul>
+
+    <button @click="readyToPlay()">Ready !</button>
+  </div>
+</template>
+
+<script>
+
+import {ref} from "vue";
+
+class Player {
+
+  constructor(name) {
+    this.name = name;
+    this.points = 0;
+  }
+
+}
+
+export default {
+  name: 'Players',
+  data () {
+    return {
+      players: [],
+    }
+  },
+  setup () {
+    const player_name = ref("");
+    return {
+      player_name
+    };
+  },
+  methods: {
+    addPlayersName(player_name) {
+      this.players.push(new Player(player_name));
+      this.player_name = '';
+    }
+  }
+}
+
+</script>

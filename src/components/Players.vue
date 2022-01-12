089734d41eedb84affdac323c669f5a420be8242
nicolas <nicolas.sabas@epitech.eu>
2022-01-12 01:52:56 +0100

    logic finished miss design from now

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,14 +1,14 @@
-<template>
-  <div id="players">
+<template v-if="isHidden">
+  <div>
     <h1>Enter players names :</h1>
 
     <input type="text" placeholder="Nom" v-model="player_name">
     <button @click="addPlayersName(player_name)" >Add</button>
     <ul>
-      <li v-for="player in players" :key="player">{{ player }} <button @click="players.splice(players.indexOf(player), 1)" >delete</button></li>
+      <li v-for="player in players" :key="player">{{ player.name }} <button @click="players.splice(players.indexOf(player), 1)" >delete</button></li>
     </ul>
 
-    <button @click="readyToPlay()">Ready !</button>
+    <button @click="readyToGame">Ready !</button>
   </div>
 </template>
 
@@ -16,20 +16,46 @@
 
 import {ref} from "vue";
 
-class Player {
-
+export class Player {
   constructor(name) {
     this.name = name;
     this.points = 0;
+    this.isKomo = false;
   }
 
+  addPoints(points){
+    if ((this.points + points) % 50 === 0){
+      this.points = 0;
+    } else {
+      this.points += points;
+    }
+  }
+
+  setKomo(isKomo){
+    this.komo = isKomo;
+  }
+
+  getPoints(){
+    return this.points;
+  }
+
+  winKomo(){
+    this.addPoints(-10);
+  }
+
+  looseKomo(){
+    this.addPoints(20);
+  }
 }
 
 export default {
   name: 'Players',
+  props: {
+    isHidden: Boolean
+  },
   data () {
     return {
-      players: [],
+      players: []
     }
   },
   setup () {
@@ -42,6 +68,11 @@ export default {
     addPlayersName(player_name) {
       this.players.push(new Player(player_name));
       this.player_name = '';
+    },
+    readyToGame(){
+      if (this.players.length > 0){
+        this.$emit('start-game', this.players);
+      }
     }
   }
 }
